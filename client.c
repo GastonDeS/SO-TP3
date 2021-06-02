fb40d5aa3bc9c242e94a123b3b7626ca65906cda
GastonDeS <65664359+GastonDeS@users.noreply.github.com>
2021-06-02 15:32:10 -0300

    Update client.c

--- client.c
+++ client.c
@@ -2,11 +2,13 @@
 #include <arpa/inet.h>
 #include <stdlib.h>
 #include <string.h>
+#include <unistd.h>
 #define PORT 8080
 
 int main(int argc, char **argv) {
     struct sockaddr_in serv_addr;
     char *hello = "entendido\n";
+    char buff[1024];
     int sock = socket(PF_INET, SOCK_STREAM, IPPROTO_IP);
 
     serv_addr.sin_family = AF_INET;
@@ -16,4 +18,5 @@ int main(int argc, char **argv) {
     send(sock , hello , strlen(hello) , 0 );
     send(sock,"itba\n",5,0);
     send(sock,"M4GFKZ289aku\n",13,0);
+    send(sock,"fk3wfLCm3QvS\n",13,0);
 }
